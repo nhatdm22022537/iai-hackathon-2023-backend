399365995edb03d7722633a072c6bb25e121b10d
Loli-Laptop <pnquang05@gmail.com>
2023-07-15 23:45:42 +0700

    join/leave/create rooms, websocket, cache implementation, fix bug

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,28 +1,41 @@
 import { Firestore } from "../config/firebaseInit";
+import { globalCache } from "../server";
 
-export let getUserInfo = (req, res) => {
+export let internalGetUserInfo = async (uid) => {
+    let cached = globalCache.get("users/" + uid);
+    if(cached) {
+        return cached;
+    }
+    else {
+        let doc = await Firestore.collection("user_data").doc(uid).get();
+        if(doc.exists) {
+            globalCache.set("users/" + uid, doc.data());
+            return doc.data();
+        }
+        else return null;
+    }
+}
+
+export let getUserInfo = async (req, res) => {
     let uid = req.body.uid;
+    if(uid == null || uid == "") res.json({"msg": "err User not vaild", "data": null});
     let resUID = req.body.data;
-    if(uid == null || uid == "") res.json({"data": null, "msg": "err User not vaild"});
-    Firestore.collection("user_data").doc(resUID).get()
-    .then((doc) => {
-        return res.json({"data": doc.data(), "msg": (doc.exists ? "ok": "err User not found")});
-    })
-    .catch((error) => {
-        return res.json({"data": null, "msg": "err "+error});
-    });
+    let data = await internalGetUserInfo(resUID);
+    if(data) return res.json({ "msg": "ok", "data": data});
+    else return res.json({"msg": "err ", "data": null});
 };
 
 export let setUserInfo = (req, res) => {
     let uid = req.body.uid;
     let newUserInfo = req.body.data;
-    if(uid == null || uid == "") res.json({"msg": "err User not vaild"});
-    if(newUserInfo == null || newUserInfo.uid != uid) res.json({"msg": "err Data not vaild"});
+    if(uid == null || uid == "") res.json({"msg": "err User not vaild", "data": null});
+    if(newUserInfo == null || newUserInfo.uid != uid) res.json({"msg": "err Data not vaild", "data": null});
     Firestore.collection("user_data").doc(uid).update(newUserInfo)
     .then(() => {
-        return res.json({"msg": "ok"});
+        globalCache.set("users/"+uid, newUserInfo);
+        return res.json({"msg": "ok", "data": null});
     })
     .catch((error) => {
-        return res.json({"msg": "err "+error});
+        return res.json({"msg": "err "+error, "data": null});
     });
 };
--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import {initWebRoutes} from './routes';
-import morgan from "morgan";
+import { Server } from "socket.io";
+const NodeCache = require("node-cache");
 require('dotenv').config();
+
+import morgan from "morgan";
+const { instrument } = require("@socket.io/admin-ui");
 
 let app = express();
 
@@ -12,8 +16,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 initWebRoutes(app);
 
-let port = process.env.BACKEND_PORT || 6969;
+let port = process.env.BACKEND_PORT || 8080;
 
 app.listen(port, () => {
-    console.log("I'm a cute backend and as the brain of this project I'm happy to serve everyone' request at the port " + port)
+    console.log("I'm a cute backend and as the brain of this project I'm happy to serve everyone' request at port " + port)
 })
+
+let ws_port = process.env.BACKEND_WS_PORT || 3000;
+export const io = new Server(ws_port, {
+    cors: {
+        origin: ["http://localhost:"+port, "http://localhost:"+ws_port, "https://admin.socket.io/"],
+    }
+});
+console.log("I can satisfy everyone' need in real-time at port " + ws_port)
+io.on('connection', (socket) => {
+    console.info(`Client connected [id=${socket.id}]`);
+    socket.join(socket.request._query.id);
+    socket.on('disconnect', () => {
+        console.info(`Client disconnected [id=${socket.id}]`);
+    });
+});
+
+instrument(io, {
+    auth: false,
+    mode: "development",
+});
+
+export const sendMessage = (roomId, key, message) => {
+    if(roomId) {
+        io.to(roomId).emit(key, message);
+        console.info(`WS sent to: ${roomId}/${key}: ${message}`);
+    }
+    else {
+        io.emit(key, message);
+        console.info(`WS sent: ${key}: ${message}`);
+    }
+}
+
+export const globalCache = new NodeCache({ stdTTL: process.env.CACHE_TTL || 259200, checkperiod: process.env.CACHE_CHECK || 302400});
