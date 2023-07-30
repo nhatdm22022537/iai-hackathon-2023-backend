2739099475fc8488b244a26718da90167838ff0f
Loli-Laptop <pnquang05@gmail.com>
2023-07-30 15:43:29 +0700

    Calculate points, fix critical bug

--- src/WSEventListener.js
+++ src/WSEventListener.js
@@ -17,23 +17,31 @@ console.log("I can satisfy everyone' need in real-time at port " + ws_port);
 io.on("connection", (socket) => {
     let uid = "";
     let rid = "";
+    let points = 0;
+    let correctStreak = 0;
+    let incorStreak = 0;
+    let timestamp = 0;
     let busy = false;
     console.info(`[id=${socket.id}] Client connected`);
     socket.join(socket.request._query.id);
 
-    socket.on("post-joinRoom", (rquid, rqrid) => {
-        console.info(`[sid=${socket.id} | uid=${rquid} | rid=${rqrid}] Joined room.`);
-        gameCtrl.internalUpdateSockID(rquid, socket.id, rqrid);
+    const logInfo = (msg, sv = false) => {
+        if (sv) console.info(`[rid=${rid} | server]: ${msg}.`);
+        else console.info(`[rid=${rid} | uid=${uid} | sid=${socket.id}]: ${msg}.`);
+    };
+    socket.on("post-joinRoom", async (rquid, rqrid) => {
         uid = rquid;
         rid = rqrid;
         socket.join(rqrid);
+        await gameCtrl.internalUpdateOnlineStatus(true, uid, rid);
+        logInfo("Joined room");
         io.to(rid).emit("get-state", uid, true);
         busy = false;
     });
 
     socket.on("post-ready", (status) => {
-        console.info(`[sid=${socket.id} | uid=${uid} | rid=${rid}] Ready phase ${status}`);
-        gameCtrl.internalUpdateReadyStatus(socket.id, status).then(async (res) => {
+        gameCtrl.internalUpdateReadyStatus(status, uid, rid).then(async (res) => {
+            logInfo(`Ready phase ${status}`);
             if (res) socket.to(rid).emit("get-ready", uid, status);
             busy = false;
         });
@@ -42,17 +50,19 @@ io.on("connection", (socket) => {
     const allReady = async (event) => {
         busy = true;
         // im busy chotto matte
-        let retries = 100; // wait for 10s, thats it
-        while (busy && retries) {
+        let retries = 200; // wait for 10s, thats it
+        while (busy && retries > 0) {
             retries -= 1;
-            await new Promise((resolve) => setTimeout(resolve, 100));
+            await new Promise((resolve) => setTimeout(resolve, 50));
         }
         const listenForAllReady = globalCache.get("listenForAllReady/"+rid);
         if (listenForAllReady) {
-            gameCtrl.internalUpdateAllReady(socket.id, 2).then((status) => {
+            gameCtrl.internalCheckAllReady(2, rid).then((status) => {
                 if (status) {
+                    logInfo("Game start phase 2", true);
                     io.to(rid).emit("get-start", 2);
-                    globalCache.set("listenForAllReady/"+rid, false);
+                    globalCache.del("listenForAllReady/"+rid);
+                    socket.offAny(allReady);
                 }
             });
         }
@@ -61,31 +71,54 @@ io.on("connection", (socket) => {
     socket.onAny(allReady);
 
     socket.on("post-start", () => {
-        console.info(`[sid=${socket.id} | uid=${uid} | rid=${rid}] Start`);
-        gameCtrl.startGame(socket.id).then(async (status) => {
+        gameCtrl.internalStartGame(uid, rid).then(async (status) => {
             if (status) {
+                logInfo("Game start phase 1", true);
                 io.to(rid).emit("get-start", 1);
-                await internalGetCorrectAnswer(rid); // starting to fetch answer
+                await gameCtrl.internalGetCorrectAnswer(rid); // starting to fetch answer
                 globalCache.set("listenForAllReady/"+rid, true); // only listen when server fully loaded
             }
             busy = false;
         });
     });
 
+    socket.on("post-startQues", (noQues) => {
+        logInfo(`Question ${noQues}, started`);
+        timestamp = Date.now();
+    });
+
     socket.on("post-answer", (noQues, ans) => {
-        console.info(`[sid=${socket.id} | uid=${uid} | rid=${rid}] Question ${noQues}, chose ${ans}`);
-        gameCtrl.internalCheckAns(socket.id, noQues, ans).then((status) => {
+        gameCtrl.internalCheckAns(noQues, ans, uid, rid).then(async (status) => {
             socket.emit("get-answer", noQues, status);
+            const timeTaken = Date.now() - timestamp;
+            const delta = await gameCtrl.internalCalcPoint(uid, rid, status, correctStreak, incorStreak, timeTaken);
+            logInfo(`Question ${noQues}, chose ${ans} in ${timeTaken}ms, ${status ? "" : "in"}correct, ${delta} points`);
+            points += delta;
+            if (status) {
+                correctStreak += 1;
+                incorStreak = 0;
+            } else {
+                incorStreak += 1;
+                correctStreak = 0;
+            }
+            logInfo("Player info updated");
+            io.to(rid).emit("get-playerData", uid, correctStreak, points);
             busy = false;
         });
     });
 
     socket.on("disconnect", () => {
-        gameCtrl.internalUpdateOnlineStatus(socket.id, false);
+        gameCtrl.internalUpdateOnlineStatus(false, uid, rid);
         io.to(rid).emit("get-state", uid, false);
-        console.info(`[id=${socket.id}] Client disconnected`);
+        logInfo("Disconnected");
+        socket._cleanup();
+        socket.removeAllListeners();
+        socket.disconnect(true);
         busy = false;
     });
+
+    // when entire room is done
+    // io.to(rid).disconnectSockets();
 });
 
 instrument(io, {
